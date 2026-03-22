8086300399b688cde93842b9180f68c2c5f9bdb6
xiaohuihuib <3083869156@qq.com>
2026-03-22 10:33:41 +0800

    Fix formatting issues in uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -246,6 +246,7 @@ const maintenances: MaintenanceConfig[] = [
     // [Optional] color of the maintenance alert at status page, default to "yellow"
     color: 'yellow',
   },
+]
 
 // Don't edit this line
 export { maintenances, pageConfig, workerConfig }
