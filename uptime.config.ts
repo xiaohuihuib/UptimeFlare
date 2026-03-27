e961a4d200d4fc0c1f4ea63ab259b9c75f9c62f8
xiaohuihuib <3083869156@qq.com>
2026-03-27 20:36:18 +0800

    Delete maintenance configurations from uptime.config.ts
    Removed maintenance configurations for 'bbs' and 'blog' monitors.

--- uptime.config.ts
+++ uptime.config.ts
@@ -143,16 +143,6 @@ const workerConfig: WorkerConfig = {
       expectedCodes: [200],
       timeout: 10000,
     },
-        {
-      id: 'bbs',
-      name: '小辉辉b的论坛（维护中）',
-      method: 'HEAD',
-      target: 'https://bbs.xiaohuihuib.dpdns.org/',
-      statusPageLink: 'https://bbs.xiaohuihuib.dpdns.org/',
-      hideLatencyChart: false,
-      expectedCodes: [200],
-      timeout: 10000,
-    },
     {
       id: 'subscription',
       name: '订阅管理系统',
@@ -291,38 +281,7 @@ const workerConfig: WorkerConfig = {
 // Also, related downtime notifications will be skipped (if any)
 // Of course, you can leave it empty if you don't need this feature
 
-const maintenances: MaintenanceConfig[] = [
-  {
-    // [Optional] Monitor IDs to be affected by this maintenance
-    monitors: ['blog'],
-    // [Optional] default to "Scheduled Maintenance" if not specified
-    title: '博客框架Flare Stack Blog1.4.0升级后故障无法查看文章',
-    // Description of the maintenance, will be shown at status page
-    body: '博客框架Flare Stack Blog1.4.0升级后故障无法查看文章，等待修复',
-    // Start time of the maintenance, in UNIX timestamp or ISO 8601 format
-    start: '2025-03-22T10:00:00+08:00',
-    // [Optional] end time of the maintenance, in UNIX timestamp or ISO 8601 format
-    // if not specified, the maintenance will be considered as on-going
-    end: '2025-03-22T12:00:00+08:00',
-    // [Optional] color of the maintenance alert at status page, default to "yellow"
-    color: 'yellow',
-  },
-  {
-    // [Optional] Monitor IDs to be affected by this maintenance
-    monitors: ['bbs'],
-    // [Optional] default to "Scheduled Maintenance" if not specified
-    title: '小辉辉b的论坛维护',
-    // Description of the maintenance, will be shown at status page
-    body: '更换架构为PHPBB，需要停机维护',
-    // Start time of the maintenance, in UNIX timestamp or ISO 8601 format
-    start: '2025-03-27T20:00:00+08:00',
-    // [Optional] end time of the maintenance, in UNIX timestamp or ISO 8601 format
-    // if not specified, the maintenance will be considered as on-going
-    end: '2025-03-29T22:00:00+08:00',
-    // [Optional] color of the maintenance alert at status page, default to "yellow"
-    color: 'yellow',
-  },
-]
+const maintenances: MaintenanceConfig[] = []
 
 // Don't edit this line
 export { maintenances, pageConfig, workerConfig }
